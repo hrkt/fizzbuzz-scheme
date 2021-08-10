6913cd7375d5200203a968c2f8d959bdebcb0e2d
hrkt <hiroki110@gmail.com>
2021-08-11 06:15:34 +0900

    change samples for using define-func syntax

--- sample/fibonacci.scm
+++ sample/fibonacci.scm
@@ -1,9 +1,4 @@
-;(define (fib n)
-;  (if (< n 2)
-;    n
-;    (+ (fib (- n 2)) (fib (- n 1)))))
-
-(define fib (lambda (n) (if (< n 2)
+(define (fib n) (if (< n 2)
     n
-    (+ (fib (- n 2)) (fib (- n 1))))))
+    (+ (fib (- n 2)) (fib (- n 1)))))
 
--- sample/fizzbuzz.scm
+++ sample/fizzbuzz.scm
@@ -1,18 +1,35 @@
-(define fb
-	(lambda (x) 
-		(if 
-			(and (= (mod x 3) 0) (= (mod x 5) 0))
-			"fizzbuzz"
+; (define fb
+; 	(lambda (x) 
+; 		(if 
+; 			(and (= (mod x 3) 0) (= (mod x 5) 0))
+; 			"fizzbuzz"
+; 			(if
+; 				(= (mod x 3) 0)
+; 				"fizz"
+; 				(if
+; 					(= (mod x 5) 0)
+; 					"buzz"
+; 					x
+; 				)
+; 			)
+; 		)
+; 	)
+; )
+; (define fizzbuzz (lambda (x) (display (fb x))))
+
+(define (fb x)
+	(if
+		(and (= (mod x 3) 0) (= (mod x 5) 0))
+		"fizzbuzz"
+		(if
+			(= (mod x 3) 0)
+			"fizz"
 			(if
-				(= (mod x 3) 0)
-				"fizz"
-				(if
-					(= (mod x 5) 0)
-					"buzz"
-					x
-				)
+				(= (mod x 5) 0)
+				"buzz"
+				x
 			)
 		)
 	)
 )
-(define fizzbuzz (lambda (x) (display (fb x))))
+(define (fizzbuzz x) (display (fb x)))
