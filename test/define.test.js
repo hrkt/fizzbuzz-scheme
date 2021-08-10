f28fa87c04a039bdf7b43ff8708dd470b2783d33
hrkt <hiroki110@gmail.com>
2021-08-11 06:18:12 +0900

    add test cases

--- test/define.test.js
+++ test/define.test.js
@@ -30,3 +30,15 @@ test('evaluating (define b 1) then (+ 1 b)', () => {
   fbs.eval('(define b 1)')
   util.codeEvaledTo('(+ 1 b)', new FsNumber(2), fbs)
 })
+
+test('evaluating (define (x2 x) (* x 2)) then (x2 1) yields 2', () => {
+  const fbs = new FizzBuzzScheme()
+  fbs.eval('(define (x2 x) (* x 2))')
+  util.codeEvaledTo('(x2 1)', new FsNumber(2), fbs)
+})
+
+test('evaluating (define (x3 x y) (+ x y)) then (x3 x y) yields 3', () => {
+  const fbs = new FizzBuzzScheme()
+  fbs.eval('(define (x3 x y) (+ x y))')
+  util.codeEvaledTo('(x3 1 2)', new FsNumber(3), fbs)
+})
