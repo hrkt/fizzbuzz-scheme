098fd44fc59b79ea08c3bd4ad82f6a44c918477e
hrkt <hiroki110@gmail.com>
2021-09-09 03:11:18 +0900

    add pair (first attempt), with 5 r5rs test case

---
+++ test/pair.test.js
@@ -0,0 +1,14 @@
+'use strict'
+
+import { FizzBuzzScheme as FBS } from '../src/index.js'
+import { FsNumber, FsPair } from '../src/sexp.js'
+
+test('toString pair object with car=1, cons=2 yields (1 . 2)', () => {
+  const p = new FsPair(new FsNumber(1), new FsNumber(2))
+  expect(p.toString()).toBe('(1 . 2)')
+})
+
+test('evaluating (cons 1 2) yields (1 . 2)', () => {
+  const code = '(cons 1 2)'
+  expect(new FBS().eval(code)).toStrictEqual(new FsPair(new FsNumber(1), new FsNumber(2)))
+})
