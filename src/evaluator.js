74b35514456c5f4e94b93441d30d5e66f4d4350c
hrkt <hiroki110@gmail.com>
2021-09-26 05:52:09 +0900

    fix bug in (define (lambda

--- src/evaluator.js
+++ src/evaluator.js
@@ -76,19 +76,26 @@ export class FsEvaluator {
               // eval args, store them, bind them as given param symbol.
               // then eval its body in the next loop.
               const evaled = []
+              // given params are in sexp.slice(1)
               for (let i = 1; i < sexp.length; i++) {
                 evaled.push(FsEvaluator.eval(sexp.at(i), env))
               }
               innerEnv.set(p.params, new FsList(evaled))
-              sexp = p.body
+              sexp = p.body.at(0) // TODO: multiplue bodies
               env = innerEnv
             } else {
+              const givenParams = sexp.slice(1)
               // ex. (lambda (x) (+ 1 2))
-              for (let i = 0; i < p.params.length; i++) {
-                innerEnv.set(p.params.at(i), FsEvaluator.eval(sexp.at(i + 1), env))
+              // fixed number "n" case or take "n or more" case
+              if (p.params.type === 'fslist') {
+                for (let i = 0; i < p.params.length; i++) {
+                  innerEnv.set(p.params.at(i), FsEvaluator.eval(givenParams.at(i), env))
+                }
+                sexp = p.body.at(0) // TODO: multiplue bodies
+                env = innerEnv
+              } else {
+                throw new Error('not implemented')
               }
-              sexp = p.body
-              env = innerEnv
             }
           } else {
           // evaled.length = sexp.length - 1 // this line slows execution, so we do not do this.
--- src/sexp.js
+++ src/sexp.js
@@ -71,12 +71,6 @@ export class FsIf extends FsSExp {
    */
   static proc (list, env) {
     throw new FsError('do not call me.')
-    // const [test, conseq, alt] = list
-    // if (FsEvaluator.eval(test, env).value) {
-    //   return FsEvaluator.eval(conseq, env)
-    // } else {
-    //   return FsEvaluator.eval(alt, env)
-    // }
   }
 }
 
@@ -90,14 +84,7 @@ export class FsLambda extends FsSExp {
     // case 2. <v> ; any number of arguments
     // case 3. (<v1> <v2> ... <vn> . <vn+1>) ; takes n or more arguments
 
-    let body = null
-    if (params instanceof FsSymbol) {
-      // case 2.
-      body = list.at(1)
-    } else {
-      body = list.at(1)
-      // body = list.slice(1)
-    }
+    const body = list.slice(1)
     const procedure = new FsDefinedProcedure(params, body, env)
     return procedure
   }
@@ -125,21 +112,6 @@ export class FsDefinedProcedure extends FsSExp {
    */
   proc (execParams) {
     throw new FsError('do not call me.')
-    // const innerEnv = new FsEnv(this.env)
-    // if (!Array.isArray(execParams)) {
-    //   throw new Error('arg type do not match')
-    // }
-    // if (this.params instanceof FsSymbol) {
-    //   // ex. ((lambda x x) 3 4 5 6)
-    //   innerEnv.set(this.params, new FsList(execParams))
-    //   return FsEvaluator.eval(this.params, innerEnv)
-    // } else {
-    //   // ex. (lambda (x) (+ 1 2))
-    //   for (let i = 0; i < this.params.length; i++) {
-    //     innerEnv.set(this.params[i], execParams[i])
-    //   }
-    //   return FsEvaluator.eval(this.body, innerEnv)
-    // }
   }
 
   toString () {
@@ -181,30 +153,26 @@ export class FsDefine extends FsSExp {
   static proc (list, env) {
     ensureListContainsTwo(list)
     const car = list.at(0)
-    const cdr = list.at(1)
-
-    if (log.getLevel() <= log.levels.DEBUG) {
-      log.debug('car:' + car + ',cdr:' + cdr)
-      log.debug('DUMP-IN-DEFINE car')
-      console.dir(car)
-      log.debug('DUMP-IN-DEFINE cdr')
-      console.dir(cdr)
-    }
 
     if (!(car instanceof FsList)) {
       // ex)
       // (define x1 (lambda (x) (* x 2)))
+      const cdr = list.at(1)
       env.set(car, FsEvaluator.eval(cdr, env))
+      if (log.getLevel() <= log.levels.DEBUG) {
+        log.debug('define symbol - symbol:' + car + ' value:' + cdr)
+      }
       return car
     } else {
       // ex)
       // (define (x2 x) (* x 2))
       const funcName = car.at(0)
       const params = car.slice(1)
+      const cdr = list.slice(1)
       const body = cdr
       const procedure = new FsDefinedProcedure(params, body, env)
       if (log.getLevel() <= log.levels.DEBUG) {
-        log.debug('define funciton - funcName:' + funcName + ' procedure:' + procedure)
+        log.debug('define functiton - funcName:' + funcName + ' procedure:' + procedure)
       }
       env.set(funcName, procedure)
       return funcName
