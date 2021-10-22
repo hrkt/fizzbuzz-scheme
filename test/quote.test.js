b37aac642a50bbba15644ebd8895fd1f3349e6f6
hrkt <hiroki110@gmail.com>
2021-10-23 08:11:38 +0900

    change import

--- test/quote.test.js
+++ test/quote.test.js
@@ -1,33 +1,28 @@
 'use strict'
 
-import { FizzBuzzScheme } from '../src/index.js'
+import { FizzBuzzScheme as FBS } from '../src/index.js'
 
 test('evaluating (quote 1) yields 1', () => {
   const code = '(quote 1)'
-  const fbs = new FizzBuzzScheme()
-  expect(fbs.eval(code).toString()).toBe('1')
+  expect(new FBS().eval(code).toString()).toBe('1')
 })
 
 test('evaluating (quote (+ 1 1)) yields (+ 1 1)', () => {
   const code = '(quote (+ 1 1))'
-  const fbs = new FizzBuzzScheme()
-  expect(fbs.eval(code).toString()).toBe('(+ 1 1)')
+  expect(new FBS().eval(code).toString()).toBe('(+ 1 1)')
 })
 
 test('evaluating \'a yields a', () => {
   const code = '\'a'
-  const fbs = new FizzBuzzScheme()
-  expect(fbs.eval(code).toString()).toBe('a')
+  expect(new FBS().eval(code).toString()).toBe('a')
 })
 
 test('evaluating \'\'a yields \'a', () => {
   const code = '\'\'a'
-  const fbs = new FizzBuzzScheme()
-  expect(fbs.eval(code).toString()).toBe('\'a')
+  expect(new FBS().eval(code).toString()).toBe('\'a')
 })
 
 test('evaluating \'\'\'a yields \'\'a', () => {
   const code = '\'\'\'a'
-  const fbs = new FizzBuzzScheme()
-  expect(fbs.eval(code).toString()).toBe('\'\'a')
+  expect(new FBS().eval(code).toString()).toBe('\'\'a')
 })
