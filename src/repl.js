d5c89d9eccac7eb1e3f609935723fed82bf2cc61
hrkt <hiroki110@gmail.com>
2021-10-24 21:19:26 +0900

    remove loading-sample "easy-mode"

--- src/repl.js
+++ src/repl.js
@@ -1,6 +1,5 @@
 // REPL tool
 'use strict'
-import FS from 'fs'
 import log from 'loglevel'
 import PromptSync from 'prompt-sync'
 
@@ -10,23 +9,6 @@ log.setLevel('info')
 
 const promptSync = PromptSync({ sigint: true })
 
-// load samples, ignoring the files that contain ';SAMPLE_IGNORE' comment
-function loadSamples (fbs) {
-  try {
-    FS.readdirSync('sample').forEach(file => {
-      const data = FS.readFileSync('sample/' + file, 'utf8')
-      log.debug('read:' + data)
-      if (!data.includes(';SAMPLE_IGNORE')) {
-        fbs.eval(data)
-      } else {
-        log.debug('ignored:' + file)
-      }
-    })
-  } catch (err) {
-    console.error(err)
-  }
-}
-
 let loop = true
 let fbs = new FBS()
 while (loop) {
@@ -35,8 +17,6 @@ while (loop) {
     // do nothing
   } else if (exp === 'clear') {
     fbs = new FBS()
-  } else if (exp === 'samples') {
-    loadSamples(fbs)
   } else if (exp === 'd') {
     fbs.enableDebugMode()
     // fbs.eval('(define fib (lambda (n) (if (< n 2) n (+ (fib (- n 2)) (fib (- n 1))))))')
